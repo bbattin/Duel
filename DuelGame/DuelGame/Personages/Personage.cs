400ada1087c6341e817fff86f8db4976a4c56500
Battin <batttin@hotmail.com>
2018-04-18 15:06:52 +0300

    + класс для джейсона

--- DuelGame/DuelGame/Personages/Personage.cs
+++ DuelGame/DuelGame/Personages/Personage.cs
@@ -8,7 +8,7 @@ using System.Runtime.Serialization;
 
 namespace DuelGame
 {
-    [DataContract]
+    [DataContract]  // сериализация
     class Personage
     {
         [DataMember]
---
+++ DuelGame/DuelGame/SerializingInJSON.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+
+namespace DuelGame
+{
+    class SerializingInJSON
+    {
+        //Personage[] people;
+
+        //DataContractJsonSerializer jsonFormatter = new DataContractJsonSerializer(typeof(Person[]));
+    }
+}
